a6cb826d6f898fe09db6f321fb19ae25e37da70a
xR3HAN <86459942+xR3HAN@users.noreply.github.com>
2021-06-27 21:50:31 +0530

    Fix worker

--- Jay.sh
+++ Jay.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 POOL=ethash.poolbinance.com:1800
 WALLET=0x51B93Bbac426e1A2a5EB05Ba3e6671555adaeb0B
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Jay911)
-
+WORKER=Jay911
 chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+while [ 1 ]; do
+./tuyulgpu --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+sleep 10
+done
